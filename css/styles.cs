8d2261412b7d41d4a764d69a0910cd72131cc7ab
alexdesosa <47509346+alexdesosa@users.noreply.github.com>
2024-02-27 02:36:29 -0300

    Nuevo proyecto para la entrega final

---
+++ css/styles.cs
@@ -0,0 +1,183 @@
+* {
+  font-family: "Bree Serif", serif;
+}
+
+.enlaces-header {
+  color: rgb(0, 0, 0);
+  font-size: 1, 5rem;
+}
+.enlaces-header:hover {
+  color: rgb(240, 248, 255);
+}
+
+.main-p {
+  padding-top: 4rem;
+  font-size: bold;
+  color: rgb(214, 214, 214);
+}
+
+.main {
+  font-size: large;
+  padding: 5rem;
+  background-color: rgb(0, 0, 0);
+}
+
+.carousel-item {
+  height: 100vh;
+  min-height: 300px;
+}
+
+.carousel-item img {
+  background-size: cover;
+}
+
+.carousel-caption {
+  bottom: 40%;
+}
+
+.carousel-caption h5 {
+  font-size: 45px;
+  text-transform: uppercase;
+  margin-bottom: 25px;
+  letter-spacing: 1.7px;
+}
+
+.carousel-caption p {
+  width: 100%;
+  margin: auto;
+  font-size: 18px;
+}
+
+.navbar-light .navbar-brand {
+  color: #fff;
+  font-size: 25px;
+  font-weight: bold;
+  letter-spacing: 2px;
+  transition: color 0.3s ease;
+}
+
+.navbar-nav .nav-item .nav-link {
+  color: #fff;
+  transition: color 0.3s ease;
+}
+
+.navbar-toggler {
+  background: #fff;
+  outline: none !important;
+  font-size: 18px;
+  line-height: 0.3;
+  padding: 1px 5px;
+}
+
+.navbar-nav {
+  text-align: center;
+}
+
+.nav-link {
+  padding: 5px;
+}
+
+.ftcontainer {
+  display: flex;
+  justify-content: space-around;
+}
+
+.titulo-footer {
+  font-size: 18px;
+}
+
+.footersocial-link {
+  padding-right: 20px;
+  text-decoration: none;
+  color: #fff;
+  font-size: 1, 5rem;
+}
+.footersocial-link:hover {
+  color: rgb(0, 0, 0);
+  transition: color 0.3s ease;
+  text-decoration: none;
+}
+
+.ftli {
+  padding-bottom: 0.75rem;
+  list-style: none;
+  text-decoration: none;
+}
+.ftli:hover {
+  color: rgb(240, 248, 255);
+}
+
+.social-icons {
+  display: flex;
+  flex-direction: row;
+  font-size: bold;
+  text-decoration: none;
+  color: rgb(0, 0, 0);
+  list-style: none;
+}
+.social-icons:hover {
+  color: rgb(240, 248, 255);
+}
+
+.derechos-footer {
+  font-size: 0.65rem;
+  margin-bottom: 0rem !important;
+}
+
+.border-ft {
+  border-right: 1px solid rgb(0, 0, 0);
+  padding-right: 2rem;
+}
+
+.navbar-footer {
+  color: #fff;
+  font-size: 1rem;
+  font-weight: small;
+  letter-spacing: 2px;
+  transition: color 0.3s ease;
+  text-align: center;
+}
+
+.link-footer {
+  color: #fff;
+  transition: color 0.3s ease;
+  text-decoration: none;
+  color: #fff;
+  list-style: none;
+}
+.link-footer:hover {
+  color: rgb(0, 0, 0);
+  list-style: none;
+  text-decoration: none !important;
+}
+
+.navbar-light .navbar-brand {
+  color: #fff;
+  font-size: 25px;
+  font-weight: bold;
+  letter-spacing: 2px;
+  transition: color 0.3s ease;
+}
+
+.navbar-nav .nav-item .nav-link {
+  color: #fff;
+  transition: color 0.3s ease;
+}
+
+.navbar-toggler {
+  background: #fff;
+  outline: none !important;
+  font-size: 18px;
+  line-height: 0.3;
+  padding: 1px 5px;
+}
+
+.navbar-nav {
+  text-align: center;
+}
+
+.nav-link {
+  padding: 5px;
+}
+
+/*# sourceMappingURL=styles.cs.map */
